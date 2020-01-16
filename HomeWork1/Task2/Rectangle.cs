7949687abbb02f9d0db0a895c1889bb28f5971b0
Alexander Diachenko <alexander.diachenko@creatio.com>
2020-01-16 11:18:51 +0200

    add class to Task2

---
+++ HomeWork1/Task2/Rectangle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Rectangle
+    {
+        double side1;
+        double side2;
+
+        public Rectangle()
+        {
+
+        }
+
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+
+        public double Area
+        {
+            get
+            {
+                return AreaCalculator();
+            }
+        }
+
+        public double Perimeter
+        {
+            get
+            {
+                return PerimeterCalculator();
+            }
+        }
+
+        private double AreaCalculator()
+        {
+            return side1 * side2;
+        }
+
+        private double PerimeterCalculator()
+        {
+            return (side1 + side2) * 2;
+        }
+    }
+}
