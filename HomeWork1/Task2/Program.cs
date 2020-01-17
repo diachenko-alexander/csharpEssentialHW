7acb1dd9fc0f9e0d411b1e983e774f26c00a10d7
a.diachenko <alexdgx@gmail.com>
2020-01-17 16:23:53 +0200

    task 2

--- HomeWork1/Task2/Program.cs
+++ HomeWork1/Task2/Program.cs
@@ -10,6 +10,17 @@ namespace Task2
     {
         static void Main(string[] args)
         {
+            Console.Write("Please enter side1: ");
+            double side1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Please enter side2: ");
+            double side2 = double.Parse(Console.ReadLine());
+
+            Rectangle rect = new Rectangle(side1, side2);
+            Console.WriteLine($"Area is: {rect.Area}");
+            Console.WriteLine($"Perimetr is: {rect.Perimeter}");
+
+            Console.ReadLine();
 
         }
     }
