119e2add28f9281f4f247bcf3ea7b9ef29b66f00
a.diachenko <alexdgx@gmail.com>
2020-01-18 14:28:42 +0200

    add task 3 to hw 2

---
+++ HomeWork1/Task3/Author.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Author
+    {
+        readonly string author;
+
+        public Author (string author)
+        {
+            this.author = author;
+        }
+
+        public void Show ()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Author: " + author);
+        }
+    }
+}
---
+++ HomeWork1/Task3/Content.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Content
+    {
+        readonly string content;
+
+        public Content (string content)
+        {
+            this.content = content;
+        }
+
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Content: " + content);
+        }
+    }
+}
