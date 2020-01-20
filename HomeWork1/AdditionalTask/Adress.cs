0b46d1a26d60a3cd73f977f0ad1d1cff2ffc65f4
a.diachenko <alexdgx@gmail.com>
2020-01-20 17:14:20 +0200

    add additional task

---
+++ HomeWork1/AdditionalTask/Adress.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalTask
+{
+    class Adress
+    {
+        private int index;
+        private string country;
+        private string city;
+        private string street;
+        private string house;
+        private string appartment;
+
+        public int Index { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string House { get; set; }
+        public string Appartment { get; set; }
+    }
+}
---
+++ HomeWork1/AdditionalTask/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adress myAdress = new Adress();
+            myAdress.Index = 12345;
+            myAdress.Country = "Ukraine";
+            myAdress.City = "Kiev";
+            myAdress.Street = "Proreznaja";
+            myAdress.House = "4B";
+            myAdress.Appartment = "1a";
+
+            Console.WriteLine($"Index: {myAdress.Index}\nCountry: {myAdress.Country}\nCity: {myAdress.City}\nStreet: {myAdress.Street}\nHouse: {myAdress.House}\nAppartment: {myAdress.Appartment}");
+            Console.ReadLine();
+
+        }
+    }
+}
