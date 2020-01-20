f949371ee37c02c794d26835e0ddd5fd2f139afd
a.diachenko <alexdgx@gmail.com>
2020-01-20 22:06:29 +0200

    add task 2 to hw2

---
+++ HomeWork2/Task2/Converter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Converter
+    {
+        private double usd;
+        private double eur;
+        private double rub;
+
+        public Converter(double usd, double eur, double rub)
+        {
+            this.usd = usd;
+            this.eur = eur;
+            this.rub = rub;
+        }
+
+        public double ConvertGrnTo (double grnAmmount, string destinationCurrency)
+        {
+            double userCurrency = GetUserCurrency(destinationCurrency);
+            if (userCurrency <= 0)
+            {
+                Console.WriteLine($"Currency mast be greater than 0");
+                return 0;
+            } else
+            {
+                return grnAmmount / userCurrency;
+            }          
+        } 
+
+        public double ConvertToGrn (double currencyAmmount, string destinationCurrency)
+        {
+            return currencyAmmount * GetUserCurrency(destinationCurrency);
+        }
+
+        private double GetUserCurrency (string userInput)
+        {
+            double result = 0;
+            switch (userInput.ToLower())
+            {
+                case "usd":
+                    result = usd;
+                    break;
+                case "eur":
+                    result = eur;
+                    break;
+                case "rub":
+                    result = rub;
+                    break;
+                default:
+                    Console.WriteLine("Not supported currency");
+                    break;
+            }
+            return result;
+        }
+
+    }
+}
---
+++ HomeWork2/Task2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter USD: ");
+            double usd = double.Parse(Console.ReadLine());
+
+            Console.Write("Please enter EUR: ");
+            double eur = double.Parse(Console.ReadLine());
+
+            Console.Write("Please enter RUB: ");
+            double rub = double.Parse(Console.ReadLine());
+
+            Console.Write("Please enter Ammount: ");
+            double ammount = double.Parse(Console.ReadLine());
+
+            Console.Write("Please enter currency to convert: ");
+            string currency = Console.ReadLine();
+
+            Converter conv = new Converter(usd, eur, rub);
+
+            Console.Write("Please enter Operation\n1. TO GRN\n2. FROM GRN\n");
+            int operation = int.Parse(Console.ReadLine());
+
+            if (operation == 1)
+            {
+                Console.WriteLine(conv.ConvertToGrn(ammount, currency));
+            } else if (operation == 2)
+            {
+                Console.WriteLine(conv.ConvertGrnTo(ammount, currency));
+            }
+            
+        }
+    }
+}
