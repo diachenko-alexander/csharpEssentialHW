0a4ba5aec140c854d36d032c76e6d7975a55cb97
a.diachenko <alexdgx@gmail.com>
2020-01-25 13:24:44 +0200

    add task 4

---
+++ HomeWork2/Task4/Invoice.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Invoice
+    {
+        readonly int account;
+        readonly string customer;
+        readonly string provider;
+
+        public string Article { get; set; }
+
+        public int Quantity { get; set; }
+
+        public Invoice (int account, string customer, string provider)
+        {
+            this.account = account;
+            this.customer = customer;
+            this.provider = provider;
+        }
+
+        private double GetArticlePrise()
+        {
+            double articlePrise = 0;
+
+            switch (Article.ToLower())
+            {
+                case "mouse":
+                    articlePrise = 100;
+                    break;
+                case "keyboard":
+                    articlePrise = 300;
+                    break;
+                default:
+                    Console.WriteLine("No such Article");
+                    break;
+            }
+            return articlePrise;
+        }
+
+        public double CalculateInvoice (bool nds)
+        {
+            if (nds)
+            {
+                return (GetArticlePrise() + (GetArticlePrise() * 0.2)) * Quantity;
+            }
+            else return GetArticlePrise() * Quantity;
+        }
+    }
+}
---
+++ HomeWork2/Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice invoice = new Invoice(1, "serg", "rosetka");
+            invoice.Article = "mouse";
+            invoice.Quantity = 1;
+            Console.WriteLine($"With nds: {invoice.CalculateInvoice(true)}");
+            Console.WriteLine($"Without nds: {invoice.CalculateInvoice(false)}");
+            Console.ReadLine();
+            
+        }
+    }
+}
