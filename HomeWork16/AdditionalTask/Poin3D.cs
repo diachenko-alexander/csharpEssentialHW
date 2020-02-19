9580e498ae7d4b282aaec742c5255e73b6ab322e
Alexander Diachenko <alexander.diachenko@creatio.com>
2020-02-19 15:52:15 +0200

    add additionalTask to HW16

---
+++ HomeWork16/AdditionalTask/Poin3D.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AdditionalTask
+{
+    public struct Point3D
+    {
+        private int x, y, z;
+
+        public Point3D(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public static Point3D operator +(Point3D p1, Point3D p2)
+        {
+            return new Point3D(p1.x + p2.x, p1.y + p2.y, p1.z + p2.z);
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"x={this.x}, y={this.y} z= {this.y}");
+        }
+    }
+}
---
+++ HomeWork16/AdditionalTask/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point3D p1 = new Point3D(1, 1, 1);
+            Point3D p2 = new Point3D(2, 2, 2);
+
+            Point3D p3 = p1 + p2;
+            Console.WriteLine(p3);
+
+        }
+    }
+}
