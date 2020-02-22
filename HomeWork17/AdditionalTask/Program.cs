34f55826365ba04c956132dfb82133bf894f1072
a.diachenko <alexdgx@gmail.com>
2020-02-22 17:59:56 +0200

    add HW 17

---
+++ HomeWork17/AdditionalTask/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace AdditionalTask
+{
+    static class Calculator
+    {
+        public static dynamic Add (dynamic a, dynamic b)
+        {
+            return a + b;
+        }
+
+        public static dynamic Sub (dynamic a, dynamic b)
+        {
+            return a - b;
+        }
+
+        public static dynamic Mul(dynamic a, dynamic b)
+        {
+            return a * b;
+        }
+
+        public static dynamic Div (dynamic a, dynamic b)
+        {
+           try
+            {
+                return a / b;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+            
+        }
+    }
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculator.Add(2,2));
+            Console.WriteLine(Calculator.Sub(10,2));
+            Console.WriteLine(Calculator.Mul(3,3));
+            Console.WriteLine(Calculator.Div(10,2));
+            Console.WriteLine(Calculator.Div(10, 0));
+
+        }
+    }
+}
---
+++ HomeWork17/Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var myDictionary = new Dictionary<dynamic, dynamic>
+            {
+                {new {Key = "apple"}, new {Value = "яблок" } },
+                {new {Key = "pen"}, new {Value = "ручка" } },
+                {new {Key = "boy"}, new {Value = "мальчик" } },
+                {new {Key = "girl"}, new {Value = "девочка" } },
+                {new {Key = "good"}, new {Value = "хорошо" } },
+                {new {Key = "bad"}, new {Value = "плохо" } },
+            };
+
+            foreach (var word in myDictionary)
+            {
+                Console.WriteLine($"{word.Key} --- {word.Value}");
+            }
+        }
+    }
+}
